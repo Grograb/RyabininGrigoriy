ad25d0c331a6220f94ccc425c72230f529d32d81
315 <315@315-40>
2024-12-24 10:29:37 +0300

    Добавьте файлы проекта.

---
+++ MainProgram()/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Рябинин;
+
+namespace MainProgram__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building ps = new Building("Алтуфьевское шоссе 100", "В доме 22 этажа, красного цвета, 5 подьездов");//Рябинин 34ИС
+            ps.Show();//Рябинин 34ИС
+            Apartament b1 = new Apartament(" 99 ","Никольский Д.А.");//Рябинин 34ИС
+            ps.Library.Add(b1);//Рябинин 34ИС
+            Apartament b2 = new Apartament("282 ","Ярлыков А.М.");//Рябинин 34ИС
+            ps.Library.Add(b2);//Рябинин 34ИС
+            Apartament b3 = new Apartament("77 ", "Анашкин А.П.");//Рябинин 34ИС
+            ps.Library.Add(b3);//Рябинин 34ИС
+            Apartament b4 = new Apartament("132 ", "Шеченков А.М.");//Рябинин 34ИС
+            ps.Library.Add(b4);//Рябинин 34ИС
+            foreach (Apartament b in ps.Library)//Рябинин 34ИС
+            {
+                b.ShowAll();//Рябинин 34ИС
+            }
+            Console.WriteLine("Выполнил Рябинин Григорий 34ИС");//Рябинин 34ИС
+            Console.ReadKey();//Рябинин 34ИС
+        }
+    }
+}
---
+++ Рябинин/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Рябинин
+{
+    public class Building
+    {
+        public string Adres { get; set; }//Рябинин 34ИС
+        public string Discription { get; set; }//Рябинин 34ИС
+        public List<Apartament>Library = new List<Apartament>();//Рябинин 34ИС
+        public Building(string ad, string ds)
+        {
+            Adres = ad;
+            Discription = ds;
+        }//Рябинин 34ИС
+        public void Show()
+        {
+            Console.WriteLine($" Адрес = {Adres}, описание = {Discription}");//Рябинин 34ИС
+        }
+    }
+    public class Apartament
+    {//Рябинин 34ИС
+        public string Number { get; set; }
+        public string Owner { get; set; }
+        public Apartament(string nm, string own)//Рябинин 34ИС
+        {
+            Number = nm;
+            Owner = own;//Рябинин 34ИС
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine($" номер квартиры = {Number}, владелец = {Owner}");//Рябинин 34ИС
+        }
+    }
+}
+//Рябинин 34ИС
